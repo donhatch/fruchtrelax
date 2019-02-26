110d89d2db1a437be633edc670f34489cce5936c
Don Hatch <donhatch@google.com>
2019-02-26 02:59:58 -0800

    wip

--- relax.py
+++ relax.py
@@ -3,6 +3,7 @@
 import math
 import re
 import sys
+import time
 
 def vxs(v,s): return [x*s for x in v]  # vector times scalar
 def vpv(v,w): return [x+y for x,y in zip(v,w)]  # vector plus vector
@@ -16,20 +17,18 @@ def dist(v,w): return length(vmv(w,v))
 def relaxOnAnySizeSphere(verts,
                          edgeLengthConstraints,
                          nIters):
-  verboseLevel = 1  # manually set this to something higher to debug
+  verboseLevel = 1  # set to 2 to show one '.' per iteration
   if verboseLevel >= 1: sys.stderr.write("    in relaxOnAnySizeSphere\n")
   if verboseLevel >= 1: sys.stderr.write("      initial verts = %r\n"%(verts,))
   if verboseLevel >= 1: sys.stderr.write("      edgeLengthConstraints = %r\n"%(edgeLengthConstraints,))
   if verboseLevel >= 1: sys.stderr.write("      nIters = %r\n"%(nIters,))
+  if verboseLevel >= 1: sys.stderr.write("      iterating... ")
+  if verboseLevel >= 1: sys.stderr.flush()
 
+  t0 = time.time()
   for iIter in range(nIters):
-    if verboseLevel >= 2: sys.stderr.write("      =============================\n")
-    if verboseLevel >= 2: sys.stderr.write("      iIter=%d/%d\n"%(iIter,nIters))
-    if verboseLevel >= 2: sys.stderr.write("          before:\n")
-    if verboseLevel >= 3: sys.stderr.write("              verts = %r\n"%(verts,))
-    if verboseLevel >= 2: sys.stderr.write("              edgeLengths = %r\n"%([dist(verts[v0],verts[v1]) for v0,v1,relation,targetEdgeLength in edgeLengthConstraints if relation=="=="]))
-    if verboseLevel >= 2: sys.stderr.write("              radii = %r\n"%([length(vert) for vert in verts]))
-
+    if verboseLevel >= 2: sys.stderr.write(".")
+    if verboseLevel >= 2: sys.stderr.flush()
     # Perturb verts to try to approach edge length constraints.
     # If a vertex is being pushed/pulled by multiple edges,
     # move it to the average of where they want it to go.
@@ -43,38 +42,26 @@ def relaxOnAnySizeSphere(verts,
         # so don't contribute anything.
         continue
       # this spring wants to change (or keep) its length from currentEdgeLength to targetEdgeLength
-      if verboseLevel >= 3: sys.stderr.write("                edge (%r,%r) wants to change length from %r to %r\n"%(v0,v1,currentEdgeLength,targetEdgeLength))
       edgeCenter = lerp(verts[v0],verts[v1],.5)
       newVerts[v0] = vpv(newVerts[v0], lerp(edgeCenter, verts[v0], targetEdgeLength/currentEdgeLength))
       newVerts[v1] = vpv(newVerts[v1], lerp(edgeCenter, verts[v1], targetEdgeLength/currentEdgeLength))
       nContributions[v0] += 1
       nContributions[v1] += 1
-    if verboseLevel >= 3: sys.stderr.write("          newVerts (before divide) = %r\n"%(newVerts,))
-    if verboseLevel >= 3: sys.stderr.write("          nContributions = %r\n"%(nContributions,))
     for i in range(len(newVerts)):
       if nContributions[i] == 0:
         newVerts[i] = verts[i]
       else:
         newVerts[i] = vxs(newVerts[i], 1./nContributions[i])
     verts = newVerts
-    if verboseLevel >= 3: sys.stderr.write("          verts = %r\n"%(verts,))
-
-    if verboseLevel >= 3: sys.stderr.write("          after edge relax, before sphere project:\n")
-    if verboseLevel >= 3: sys.stderr.write("              verts = %r\n"%(verts,))
-    if verboseLevel >= 3: sys.stderr.write("              edgeLengths = %r\n"%([dist(verts[v0],verts[v1]) for v0,v1,relation,targetEdgeLength in edgeLengthConstraints if relation=="=="]))
-    if verboseLevel >= 3: sys.stderr.write("              radii = %r\n"%([length(vert) for vert in verts]))
 
     # Project (exactly) to the average-radius sphere.
     radii = [length(vert) for vert in verts]
     avgRadius = sum(radii) / len(radii)
-    if verboseLevel >= 2: sys.stderr.write("              avgRadius = %r\n"%(avgRadius,))
     verts = [vxs(vert, avgRadius/radius) for vert,radius in zip(verts,radii)]
-    if verboseLevel >= 3: sys.stderr.write("          after:\n")
-    if verboseLevel >= 3: sys.stderr.write("              verts = %r\n"%(verts,))
-    if verboseLevel >= 3: sys.stderr.write("              edgeLengths = %r\n"%([dist(verts[v0],verts[v1]) for v0,v1,relation,targetEdgeLength in edgeLengthConstraints if relation=="=="]))
-    if verboseLevel >= 3: sys.stderr.write("              radii = %r\n"%([length(vert) for vert in verts]))
-    if verboseLevel >= 2: sys.stderr.write("      =============================\n")
+  t1 = time.time()
 
+  if verboseLevel >= 1: sys.stderr.write("\n")
+  if verboseLevel >= 1: sys.stderr.write("      did %r iterations in %.6f secs.\n"%(nIters, t1-t0))
   if verboseLevel >= 1: sys.stderr.write("      final verts = %r\n"%(verts,))
   if verboseLevel >= 1: sys.stderr.write("      final spring lengths with '==' constraints: %r\n"%([dist(verts[v0],verts[v1]) for v0,v1,relation,targetEdgeLength in edgeLengthConstraints if relation=="=="]))
   if verboseLevel >= 1: sys.stderr.write("      final spring lengths with '>=' constraints: %r\n"%([dist(verts[v0],verts[v1]) for v0,v1,relation,targetEdgeLength in edgeLengthConstraints if relation==">="]))
@@ -89,15 +76,14 @@ def printSVG(verts, edges):
   sys.stderr.write("verts = %r\n"%(verts,))
   sys.stderr.write("edges = %r\n"%(edges,))
   print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>")
-  #print("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"256pt\" height=\"256pt\" viewBox=\"0 0 256 256\" transform=\"translate(128,128)\">")
-  print("<svg xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\" width=\"768pt\" height=\"256pt\" viewBox=\"0 0 768 256\" >")
+  print("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"768pt\" height=\"128pt\" viewBox=\"0 0 768 128\" >")
 
   maxRadius = max(length(vert) for vert in verts)
   scale = 50/maxRadius  # can't put this as part of the xform, or it will thicken the lines
   pointRadius = .02
 
-  translatex = 64
   # show three projections of it.
+  translatex = 64  # for starters
   for xy in [[0,1], [0,2], [1,2]]:  # xy, xz, yz.  no particular reason except they look ok for frucht.
     print(" <g style=\"fill:none;stroke:#000000\" transform=\"translate(%r,64)\" >"%(translatex,))
     for vert in verts:
@@ -117,14 +103,15 @@ def printSVG(verts, edges):
 
 
 if len(sys.argv) != 2:
-  exit("Usage: relax.py <nIters>")
+  exit("Usage: relax.py <nIters> < input.txt > output.svg")
 nIters = int(sys.argv[1])
 
 verts = []
 edgeLengthConstraints = []
 
 if sys.stdin.isatty():
-  print("Enter 3d vertex coords (3 numbers per line), and distance constraints:")
+  sys.stderr.write("Enter 3d vertex coords (3 numbers per line), and distance constraints:")
+  sys.stderr.flush()
 for line in sys.stdin:
   # strip optional comment (beginning with '#') off the end, and whitespace
   line = re.sub('#.*', '', line).strip()
